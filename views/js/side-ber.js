cd304b954c70e620d955761a34b541876dfb219f
Nishigaki2706 <cn34927nc@gmail.com>
2022-01-25 23:05:46 +0900

    不要な部分を削除

--- views/js/side-ber.js
+++ views/js/side-ber.js
@@ -7,23 +7,6 @@ $('header nav').on('click', function(){
 })
 });
 
-$(function(){
-    var top = document.getElementById('top');
-    top.addEventListener('click',() => {
-        $('.side-ber').hide();
-    })
-    // // サイドバー以外のところを押したらサイドバーが消える
-    // if(top.hasClass('active')){
-    //     var close = $('.side-ber');
-    //     var body_element = document.querySelector('html body');
-    //     body_element.on('click',function(){
-    //         top.removeClass('active');
-    //         body_element.removeClass('active');
-    //         close.hide();
-    //     });
-    // }
-})
-
 //トップに戻る、サンプルを押したらメニューバーが閉じる
 $(function(){
     $('top').on('click',function() {
