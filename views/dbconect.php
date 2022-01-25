8ed16bc2d77b052409dfb57db9ecb9cd3a2e4f7a
Nishigaki2706 <cn34927nc@gmail.com>
2022-01-25 17:23:37 +0900

    test

--- views/dbconect.php
+++ views/dbconect.php
@@ -1,14 +1,13 @@
 <?php
 // 環境変数設定
 $host = getenv('DB_HOST');
-$name = getenv('DB_NAME');
+$dbname = getenv('DB_NAME');
 $password = getenv('DB_PASSWORD');
 $user = getenv('DB_USER');
 
 try {
     // DB接続
-    $pdo = new PDO('mysql:dbname='. $name .';host='. $host .';' , $user , $password);
+    $pdo = new PDO('mysql:dbname='. $dbname .';host='. $host .';' , $user , $password);
 } catch (PDOException $e) {
     $msg = $e->getMessage();
 }
-var_dump($pdo);
