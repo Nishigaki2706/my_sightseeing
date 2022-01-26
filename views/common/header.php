e3e98139302c8137373bac5da7df408854ec943d
Nishigaki2706 <cn34927nc@gmail.com>
2022-01-26 23:40:35 +0900

    不要な部分を削除

--- views/common/header.php
+++ views/common/header.php
@@ -5,11 +5,6 @@
         </nav>
         <div class="left">
             <div class="title">My観光ページ</div>
-            <!-- <?php 
-                echo "<div class=user_name>";
-                echo htmlspecialchars($session_name) . 'さんこんにちは';
-                echo "</div>";
-            ?> -->
         </div>
         <?php 
                 echo "<div class=user_name>";
